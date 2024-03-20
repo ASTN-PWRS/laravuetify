6608c18b6871dd337d1265f2c41d8cea9f2ebdf7
Asao Kokubo <cerbera0816@gmail.com>
2024-03-20 16:41:01 +0900

    implement login

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -1,11 +1,5 @@
 <script setup>
-import Checkbox from "@/Components/Checkbox.vue";
-import GuestLayout from "@/Layouts/GuestLayout.vue";
-import InputError from "@/Components/InputError.vue";
-import InputLabel from "@/Components/InputLabel.vue";
-import PrimaryButton from "@/Components/PrimaryButton.vue";
-import TextInput from "@/Components/TextInput.vue";
-import { Head, Link, useForm } from "@inertiajs/vue3";
+import { Head, useForm } from "@inertiajs/vue3";
 
 defineProps({
     canResetPassword: {
@@ -31,67 +25,35 @@ const submit = () => {
 
 <template>
     <Head title="Log in"></Head>
-    <GuestLayout>
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
-        </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <InputLabel for="email" value="Email" />
-
-                <TextInput
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4">
-                <InputLabel for="password" value="Password" />
-
-                <TextInput
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                />
-
-                <InputError class="mt-2" :message="form.errors.password" />
-            </div>
-
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Forgot your password?
-                </Link>
-
-                <PrimaryButton
-                    class="ms-4"
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                >
-                    Log in
-                </PrimaryButton>
-            </div>
-        </form>
-    </GuestLayout>
+    <v-app>
+        <v-main>
+            <v-container class="fill-height" fluid>
+                <v-card width="600px" class="mx-auto mt-5">
+                    <v-card-title class="py-3 bg-blue"> Login </v-card-title>
+                    <v-card-actions class="py-3">
+                        <v-form style="width: 100%" @submit.prevent="submit">
+                            <v-text-field
+                                prepend-icon="mdi-account-circle"
+                                label="e-mail"
+                                density="compact"
+                            />
+                            <v-text-field
+                                prepend-icon="mdi-lock"
+                                append-icon="mdi-eye-off"
+                                label="password"
+                                density="compact"
+                            />
+                            <v-card-actions>
+                                <v-row class="px-5 justify-end">
+                                    <v-btn variant="tonal" class="bg-red"
+                                        >Login</v-btn
+                                    >
+                                </v-row>
+                            </v-card-actions>
+                        </v-form>
+                    </v-card-actions>
+                </v-card>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
